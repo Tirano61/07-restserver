fec9f8a9608cb514c29d0ec4da590fb0b0472b0e
Tirano61 <dariorama60@gmail.com>
2021-01-20 18:05:14 -0300

    variable entorno mongoDB

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe_user:ws1t6u5XK6ffbVpx@cluster0.rygsb.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
